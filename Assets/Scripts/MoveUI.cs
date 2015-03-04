d73c702c1c4225996ba63ca6a5cbde55dd9aa496
unknown <NicoBarbaros>
2015-03-04 21:44:08 +0100

    NewScript

---
+++ Assets/Scripts/MoveUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//using Random = UnityEngine.Random;
+public class MoveUI : MonoBehaviour {
+
+	public Transform target;
+	public float move = 0.0f;
+	private bool moving = false;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (moving) 
+		{	
+
+		}
+	}
+}
