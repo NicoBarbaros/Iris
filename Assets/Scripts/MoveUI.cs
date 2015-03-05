5f9180035a4f43e88984e482d8a47a3f59561408
unknown <NicoBarbaros>
2015-03-05 21:02:36 +0100

    CoreofMenuSistemImplemented

--- Assets/Scripts/MoveUI.cs
+++ Assets/Scripts/MoveUI.cs
@@ -4,16 +4,40 @@ using System.Collections;
 //using Random = UnityEngine.Random;
 public class MoveUI : MonoBehaviour {
 
-	public Transform target;
-	public float move = 0.0f;
-	private bool moving = false;
+	public float smooth;
+	public float step;
+
+	private Vector3 oldPosition;
+	private bool moved = false;
+
+	void Start()
+	{
+		oldPosition = transform.position;
+	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()                                                 
 	{
-		if (moving) 
-		{	
+		if(moved)
+		PositionChanging (step);
+		else
+			PositionChanging (0.0f);
+	} 
 
-		}
+	public void PositionChanging(float distance)
+	{
+		Vector3 newPosition = new Vector3 (oldPosition.x + distance, oldPosition.y,
+		                                   oldPosition.z);
+		
+		//newPosition = position;
+		transform.position = Vector3.Lerp (transform.position, newPosition, Time.deltaTime * smooth);
+		Debug.Log (newPosition);
 	}
+
+	public void Toogle()
+	{
+		moved = !moved;
+	}
+
+
 }
